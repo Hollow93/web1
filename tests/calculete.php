869633d8d12d1fef1caadef7a42d231fedaad8bd
Владислав Львович Быконь <Hollow-oren@yandex.ru>
2017-02-04 15:33:21 +0500

    Шаблон 2го теста

--- tests/calculete.php
+++ tests/calculete.php
@@ -11,12 +11,59 @@ namespace Tests;
 use PHPUnit_Framework_TestCase;
 
 
-class calculete extends PHPUnit_Framework_TestCase
+class Calculete extends PHPUnit_Framework_TestCase
 {
-    public function test2() {
+    public function test2()
+    {
 
-    $object = new Tester ;
-    $this->assertNotNull($object);
-}
+        $object = new Tester;
+        $this->assertNotNull($object);
+    }
+
+    public function testOne()
+    {
+
+        $object = new Tester;
+        $object->one = '13';
+        $this->assertEquals(13, $object->one);
+    }
+
+    public function testSumm()
+    {
+        $object = new Tester;
+        $object->one = '13';
+        $object->two = '24';
+        $this->assertEquals(13, $object->one);
+        $this->assertEquals(24, $object->two);
+        $object->three = $object->two + $object->one;
+        $this->assertEquals(37, $object->three);
+    }
+
+    public function testSum()
+    {
+        $object = new Tester;
+
+        $object->three = $object->two + $object->one;
+        $this->assertEquals(37, $object->three);
+
+    }
+
+    public function testSumm()
+    {
+        $object = new Tester;
+        $object->one = $this->getSumme($object->one);
+        $object->two = $this->getSumme($object->two);
+        $this->assertEquals(13, $object->one);
+        $this->assertEquals(24, $object->two);
+        $object->three = $object->two + $object->one;
+        $this->assertEquals(37, $object->three);
+    }
+
+    public function getSumme(){
+
+        $object->one = 13
+        $object->two = 24
+
+    }
 
 }
